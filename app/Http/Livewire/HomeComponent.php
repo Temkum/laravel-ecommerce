c306e09653398ba61fe9ea77955dbcc2a3530b36
Kum Jude Tem <judekum14@gmail.com>
2021-05-17 01:41:30 +0100

    Made home page slider dynamic

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -11,6 +11,7 @@ class HomeComponent extends Component
     public function render()
     {
         $sliders = HomeSlider::where('status', 1)->get();
+        
         $latest_products = Product::orderBy('created_at', 'DESC')->get()->take(8);
 
         return view('livewire.home-component', ['sliders' => $sliders, 'latest_products' => $latest_products])->layout('layouts.base');
